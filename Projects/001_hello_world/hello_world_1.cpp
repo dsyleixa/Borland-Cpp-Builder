d0675dd0ed7e7b723835791d14002d2cfb4682ab
dsyleixa <29887867+dsyleixa@users.noreply.github.com>
2019-01-19 13:19:58 +0100

    Add files via upload

---
+++ Projects/001_hello_world/hello_world_1.cpp
@@ -0,0 +1,36 @@
+//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+
+#include "hello_world_1.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma resource "*.dfm"
+TForm1 *Form1;
+//---------------------------------------------------------------------------
+__fastcall TForm1::TForm1(TComponent* Owner)
+        : TForm(Owner)
+{
+}
+
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Button1Click(TObject *Sender)
+{
+   ShowMessage ("Hello World!");
+   /* alternatively:
+   Application->MessageBox ("Hello World!", "greetings", 1+64);
+   //  1: MB_OKCANCEL
+   // 64: MB_ICONINFORMATION
+   */
+}
+
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Button2Click(TObject *Sender)
+{
+    Application->Terminate();       
+}
+
+//---------------------------------------------------------------------------
